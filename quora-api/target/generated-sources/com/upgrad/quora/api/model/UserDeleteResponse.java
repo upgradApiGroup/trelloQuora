4f23ae7505183f16eec6a1eabc125cb6555eca0e
sourav.ind2 <sourav.ind2@gmail.com>
2020-07-03 22:30:57 +0530

    Added the AdminController with delete user by using userID feature. (All Tests Passed)

--- quora-api/target/generated-sources/com/upgrad/quora/api/model/UserDeleteResponse.java
+++ quora-api/target/generated-sources/com/upgrad/quora/api/model/UserDeleteResponse.java
@@ -13,7 +13,7 @@ import javax.validation.constraints.*;
  * UserDeleteResponse
  */
 @Validated
-@javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2020-07-02T23:10:21.507+05:30")
+@javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2020-07-03T13:16:06.995+05:30")
 
 public class UserDeleteResponse   {
   @JsonProperty("id")
